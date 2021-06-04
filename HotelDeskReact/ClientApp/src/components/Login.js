17a06320466ec0fc33b787389339046f3545fcb6
Sbirciog Bogdan <sbirciogbogdanovidiu@gmail.com>
2021-06-04 10:09:26 +0300

    Login and Register Comp

---
+++ HotelDeskReact/ClientApp/src/components/Login.js
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelDeskReact.ClientApp.src.components
+{
+    public class Login
+    {
+    }
+}
